9e5c142911c9b222b939c1d5d7416a6a5b7539ae
kongbin <kongbin@inspur.com>
2023-07-04 11:28:22 +0800

    deploy

---
+++ assets/guide_versions.md.1623ee42.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode } from "./chunks/framework.b5056c03.js";
+const __pageData = JSON.parse('{"title":"版本更新及日志","description":"","frontmatter":{},"headers":[{"level":2,"title":"版本说明","slug":"版本说明","link":"#版本说明","children":[]},{"level":2,"title":"0.3.1 - 2023-03-25","slug":"_0-3-1-2023-03-25","link":"#_0-3-1-2023-03-25","children":[{"level":3,"title":"里程碑🚩","slug":"里程碑🚩","link":"#里程碑🚩","children":[]},{"level":3,"title":"重要说明📣","slug":"重要说明📣","link":"#重要说明📣","children":[]},{"level":3,"title":"优化✒️","slug":"优化✒️","link":"#优化✒️","children":[]},{"level":3,"title":"修复⛏️","slug":"修复⛏️","link":"#修复⛏️","children":[]}]}],"relativePath":"guide/versions.md","filePath":"guide/versions.md"}');
+const _sfc_main = { name: "guide/versions.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="版本更新及日志" tabindex="-1">版本更新及日志 <a class="header-anchor" href="#版本更新及日志" aria-label="Permalink to &quot;版本更新及日志&quot;">​</a></h1><h2 id="版本说明" tabindex="-1">版本说明 <a class="header-anchor" href="#版本说明" aria-label="Permalink to &quot;版本说明&quot;">​</a></h2><p>版本格式：1.2.3</p><p>版本格式：主版本号.次版本号.修订号，版本号递增规则如下：</p><blockquote><p>主版本号：表示正在开发阶段；</p><p>次版本号：增加新的功能时增加；</p><p>修订号：只要有改动就增加。</p></blockquote><ul><li><p>主版本(v1)：全盘重构时；重大功能或方向改变时；大范围不兼容之前的接口时；</p></li><li><p>次版本(v1.2)：增加新的业务功能时增加；</p></li><li><p>修订号(v1.2.3)：增加新的接口时增加；在接口不变的情况下，增加接口的非必填属性时增加；增强和扩展接口功能时增加。</p></li></ul><div class="tip custom-block"><p class="custom-block-title">提示</p><ol><li>版本一经发布，不得修改其内容，任何修改必须在新版本发布！</li><li>在接口还没有确定下来的时候，应该先使用开发版本号。</li><li>业务功能 &gt; 功能 &gt; 接口</li></ol></div><h2 id="_0-3-1-2023-03-25" tabindex="-1">0.3.1 - 2023-03-25 <a class="header-anchor" href="#_0-3-1-2023-03-25" aria-label="Permalink to &quot;0.3.1 - 2023-03-25&quot;">​</a></h2><h3 id="里程碑🚩" tabindex="-1">里程碑🚩 <a class="header-anchor" href="#里程碑🚩" aria-label="Permalink to &quot;里程碑🚩&quot;">​</a></h3><h3 id="重要说明📣" tabindex="-1">重要说明📣 <a class="header-anchor" href="#重要说明📣" aria-label="Permalink to &quot;重要说明📣&quot;">​</a></h3><h3 id="优化✒️" tabindex="-1">优化✒️ <a class="header-anchor" href="#优化✒️" aria-label="Permalink to &quot;优化✒️&quot;">​</a></h3><h3 id="修复⛏️" tabindex="-1">修复⛏️ <a class="header-anchor" href="#修复⛏️" aria-label="Permalink to &quot;修复⛏️&quot;">​</a></h3>', 12);
+const _hoisted_13 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_13);
+}
+const versions = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  versions as default
+};
