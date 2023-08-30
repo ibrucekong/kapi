e90f57e91713151f011f29c8432dd326c815f0cb
kongbin <kongbin@inspur.com>
2023-08-30 10:59:22 +0800

    deploy

--- assets/guide_tools_cli.md.ef0d2c71.lean.js
+++ assets/guide_tools_cli.md.ef0d2c71.lean.js
@@ -1,5 +1,5 @@
 import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode } from "./chunks/framework.1251c745.js";
-const __pageData = JSON.parse('{"title":"脚手架","description":"","frontmatter":{},"headers":[{"level":2,"title":"KAPI是什么？","slug":"kapi是什么","link":"#kapi是什么","children":[]},{"level":2,"title":"安装","slug":"安装","link":"#安装","children":[]},{"level":2,"title":"使用","slug":"使用","link":"#使用","children":[]},{"level":2,"title":"新建项目","slug":"新建项目","link":"#新建项目","children":[]}],"relativePath":"guide/tools/cli.md","filePath":"guide/tools/cli.md"}');
+const __pageData = JSON.parse('{"title":"脚手架","description":"","frontmatter":{},"headers":[{"level":2,"title":"脚手架是什么？","slug":"脚手架是什么","link":"#脚手架是什么","children":[]},{"level":2,"title":"安装","slug":"安装","link":"#安装","children":[]},{"level":2,"title":"使用","slug":"使用","link":"#使用","children":[]},{"level":2,"title":"新建项目","slug":"新建项目","link":"#新建项目","children":[]}],"relativePath":"guide/tools/cli.md","filePath":"guide/tools/cli.md"}');
 const _sfc_main = { name: "guide/tools/cli.md" };
 const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 10);
 const _hoisted_11 = [
